8943993faed628861793d3ff49a54cac389de76b
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-05-23 22:43:46 +0300

    Initial Commit

---
+++ Lesson_3.4_(swiftUI)/Lesson_3_4__swiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_3_4__swiftUI_App.swift
+//  Lesson_3.4_(swiftUI)
+//
+//  Created by Евгения Шевцова on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_3_4__swiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
